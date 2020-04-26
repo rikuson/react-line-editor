37ad6d05f91e57df5b90e104c5d433b2fa22c083
Rikson <rikuson@users.noreply.github.com>
2020-04-26 18:06:07 +0900

    Add properties

--- src/containers/line-editor.jsx
+++ src/containers/line-editor.jsx
@@ -1,16 +1,38 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import store from '../store';
+import { createLogger } from 'redux-logger';
+import reducer from '../reducers';
 import TextField from './textfield';
 import interpreter from '../interpreter';
 import keybinder from '../keybinder';
 import eventhandler from '../eventhandler';
 
 class LineEditor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const middlewares = [];
+    if (process.env.NODE_ENV === 'development') {
+      const logger = createLogger();
+      middlewares.push(logger);
+    }
+    middlewares.push(this.props.interpreter);
+    middlewares.push(this.props.keybinder);
+    middlewares.push(this.props.eventhandler);
+
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    const store = createStoreWithMiddleware(reducer);
+
+    this.state = {
+      store,
+    };
+  }
+
   render() {
     return (
-      <Provider store={store}>
+      <Provider store={this.state.store}>
         <TextField
           style={this.props.style}
           autoFocus={this.props.autoFocus}
