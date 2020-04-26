4093f2e9d33d16bb45b051add00889ffb0d73c90
Rikson <rikuson@users.noreply.github.com>
2020-04-26 18:09:51 +0900

    Update indexjs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import LineEditor from './containers/line-editor';
 import keymap from './keymap';
 import keybinder from './keybinder';
 import interpreter from './interpreter';
+import eventhandler from './eventhandler';
 
-// NOTE: `export` gives error
 export {
   LineEditor,
   keymap,
   keybinder,
   interpreter,
+  eventhandler,
 };
